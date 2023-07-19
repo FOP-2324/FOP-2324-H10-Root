db86d4041a66713803f02d2bb24534d068726281
LWcoder02 <larsw02@gmail.com>
2023-07-19 17:39:27 +0200

    Added ListOfListsIterator and some methods in ListSolver

---
+++ src/main/java/h10/ListOfListsIterator.java
@@ -0,0 +1,15 @@
+package h10;
+
+import java.util.Iterator;
+
+public class ListOfListsIterator<T> implements Iterator<T> {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public T next() {
+        return null;
+    }
+}
--- src/main/java/h10/ListSolver.java
+++ src/main/java/h10/ListSolver.java
@@ -1,16 +1,26 @@
 package h10;
 
+import java.util.Comparator;
+
 public class ListSolver {
 
-    public static <T> ListItem<T> makeFlatListInPlace(ListItem<ListItem<T>> doubleList) {
+    public static <T> ListItem<T> makeFlatListInPlace(ListItem<ListItem<T>> listOfLists) {
         return null;
     }
 
 
-    public static <T> ListItem<T> makeFlatListAsCopy(ListItem<ListItem<T>> doubleList) {
+    public static <T> ListItem<T> makeFlatListAsCopy(ListItem<ListItem<T>> listOfLists, Comparator<T> cmp) {
         return null;
     }
 
+
+    public static <T> ListItem<ListItem<T>> makeListOfListsAsCopy(ListItem<T> lst) {
+        return null;
+    }
+
+    public static <T> ListItem<ListItem<T>> makeListOfListsInPlace(ListItem<T> lst) {
+        return null;
+    }
 
     public static <T> ListItem<T> reverseList(ListItem<T> list) {
         return null;
@@ -18,6 +28,10 @@ public class ListSolver {
 
 
     public static <T> ListItem<T> mergeLists(ListItem<T> lst1, ListItem<T> lst2, Adder<T> adder) {
+        return null;
+    }
+
+    public static <T> ListItem<T> shiftItemToLeft(ListItem<T> lst) {
         return null;
     }
 }
