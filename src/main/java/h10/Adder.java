a13f1d2222c434a5c5627bd318de6b70315f590c
LWcoder02 <larsw02@gmail.com>
2023-07-19 10:02:34 +0200

    Added "Adder" class and "ListSolver" class

---
+++ src/main/java/h10/Adder.java
@@ -0,0 +1,6 @@
+package h10;
+
+public interface Adder<T> {
+
+    public T add(T t1, T t2);
+}
---
+++ src/main/java/h10/ListSolver.java
@@ -0,0 +1,23 @@
+package h10;
+
+public class ListSolver {
+
+    public static <T> ListItem<T> makeFlatListInPlace(ListItem<ListItem<T>> doubleList) {
+        return null;
+    }
+
+
+    public static <T> ListItem<T> makeFlatListAsCopy(ListItem<ListItem<T>> doubleList) {
+        return null;
+    }
+
+
+    public static <T> ListItem<T> reverseList(ListItem<T> list) {
+        return null;
+    }
+
+
+    public static <T> ListItem<T> mergeLists(ListItem<T> lst1, ListItem<T> lst2, Adder<T> adder) {
+        return null;
+    }
+}
