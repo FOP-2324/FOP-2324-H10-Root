21f5ad1dc05a18fd16c8de904080e9d7b96b7e74
LWcoder02 <larsw02@gmail.com>
2023-08-15 13:45:03 +0200

    Implemented reverseList

--- src/main/java/h10/ListSolver.java
+++ src/main/java/h10/ListSolver.java
@@ -1,29 +1,50 @@
 package h10;
 
 import java.util.Comparator;
+import java.util.function.UnaryOperator;
 
 public class ListSolver {
 
     public static <T> ListItem<T> makeFlatListInPlace(ListItem<ListItem<T>> listOfLists) {
+        return makeFlatList(listOfLists, true);
+    }
+
+    public static <T> ListItem<T> makeFlatListAsCopy(ListItem<ListItem<T>> listOfLists) {
+        return makeFlatList(listOfLists, false);
+    }
+
+    private static <T> ListItem<T> makeFlatList(ListItem<ListItem<T>> listOfLists, boolean inPlace) {
         return null;
     }
 
+    public static <T> ListItem<ListItem<T>> makeListOfListsAsCopy(ListItem<T> lst, Comparator<T> cmp) {
+        return makeListOfLists(lst, cmp, false);
+    }
 
-    public static <T> ListItem<T> makeFlatListAsCopy(ListItem<ListItem<T>> listOfLists, Comparator<T> cmp) {
+    public static <T> ListItem<ListItem<T>> makeListOfListsInPlace(ListItem<T> lst, Comparator<T> cmp) {
+        return makeListOfLists(lst, cmp, true);
+    }
+
+    private static <T> ListItem<ListItem<T>> makeListOfLists(ListItem<T> lst, Comparator<T> cmp, boolean inPlace) {
         return null;
     }
 
-
-    public static <T> ListItem<ListItem<T>> makeListOfListsAsCopy(ListItem<T> lst) {
-        return null;
-    }
-
-    public static <T> ListItem<ListItem<T>> makeListOfListsInPlace(ListItem<T> lst) {
-        return null;
-    }
-
+    /**
+     *
+     * @param list
+     * @return
+     * @param <T>
+     */
     public static <T> ListItem<T> reverseList(ListItem<T> list) {
-        return null;
+        ListItem<T> head = list;
+        ListItem<T> p = head;
+        while(p.next != null) {
+            ListItem<T> nextItem = p.next;
+            p.next = p.next.next;
+            nextItem.next = head;
+            head = nextItem;
+        }
+        return head;
     }
 
 
@@ -33,5 +54,13 @@ public class ListSolver {
 
     public static <T> ListItem<T> shiftItemToLeft(ListItem<T> lst) {
         return null;
+    }
+
+    public static <T> ListItem<T> filterList(ListItem<T> lst, UnaryOperator<T> fct) {
+        return null;
+    }
+
+    public static <T> ListOfListsIterator<T> iterator(ListItem<ListItem<T>> listOfLists) {
+        return new ListOfListsIterator<>(listOfLists);
     }
 }
