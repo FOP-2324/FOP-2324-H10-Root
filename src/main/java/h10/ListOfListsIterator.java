27f9bcbb110d3ce11302a8ef8b4e483d9ea6b8cd
LWcoder02 <larsw02@gmail.com>
2023-08-15 13:44:26 +0200

    Implemented next and hasNext Methods.

--- src/main/java/h10/ListOfListsIterator.java
+++ src/main/java/h10/ListOfListsIterator.java
@@ -3,13 +3,43 @@ package h10;
 import java.util.Iterator;
 
 public class ListOfListsIterator<T> implements Iterator<T> {
+
+
+    private ListItem<T> current;
+    private ListItem<ListItem<T>> currentLstOfLst;
+
+    /**
+     *
+     * @param listOfLists
+     */
+    public ListOfListsIterator(ListItem<ListItem<T>> listOfLists) {
+        this.currentLstOfLst = listOfLists;
+        this.current = listOfLists.key;
+    }
+
     @Override
     public boolean hasNext() {
+        if(this.current.next != null) {
+            return true;
+        }
+        else if(this.currentLstOfLst.next != null) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public T next() {
-        return null;
+        T tmp = this.current.key;
+        if(this.current.next != null) {
+            this.current = this.current.next;
+        }
+        else {
+            if(this.currentLstOfLst.next != null) {
+                this.currentLstOfLst = this.currentLstOfLst.next;
+                this.current = this.currentLstOfLst.key;
+            }
+        }
+        return tmp;
     }
 }
