0c3234f05c5fc169ca883e341404c26c6ab274bb
LWcoder02 <larsw02@gmail.com>
2023-08-17 09:31:53 +0200

    Added new constructor

--- src/main/java/h10/ListItem.java
+++ src/main/java/h10/ListItem.java
@@ -1,6 +1,13 @@
 package h10;
 
 public class ListItem<T> {
+
     public T key;
     public ListItem<T> next;
+
+    public ListItem() {}
+
+    public ListItem(T key) {
+        this.key = key;
+    }
 }
