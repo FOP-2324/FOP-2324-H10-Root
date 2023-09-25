13117f11bb9dcbdc935db31d7f4087fa81dfe398
LWcoder02 <larsw02@gmail.com>
2023-09-25 15:04:03 +0200

    Added union and intersection methods

--- src/main/java/h10/MySet.java
+++ src/main/java/h10/MySet.java
@@ -4,27 +4,33 @@ public class MySet<T> {
 
     private ListItem<T> setList;
 
-    public ListItem<T> unionInPlace(MySet<T> set) {
+    public MySet() {}
+
+    public MySet(ListItem<T> lst) {
+        this.setList = lst;
+    }
+
+    public MySet<T> unionInPlace(MySet<T> set) {
         return union(set, true);
     }
 
-    public ListItem<T> unionAsCopy(MySet<T> set) {
+    public MySet<T> unionAsCopy(MySet<T> set) {
         return union(set, false);
     }
 
-    private ListItem<T> union(MySet<T> set, boolean inPlace) {
+    private MySet<T> union(MySet<T> set, boolean inPlace) {
         return null;
     }
 
-    public ListItem<T> intersectionInPlace(MySet<T> set) {
+    public MySet<T> intersectionInPlace(MySet<T> set) {
         return intersection(set, true);
     }
 
-    public ListItem<T> intersectionAsCopy(MySet<T> set) {
+    public MySet<T> intersectionAsCopy(MySet<T> set) {
         return intersection(set, false);
     }
 
-    private ListItem<T> intersection(MySet<T> set, boolean inPlace) {
+    private MySet<T> intersection(MySet<T> set, boolean inPlace) {
         return null;
     }
 
