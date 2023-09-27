7f5dc5de69589105860de64a95f6f766055830ef
LWcoder02 <larsw02@gmail.com>
2023-09-27 10:03:28 +0200

    Changed small test

--- src/main/java/h10/Main.java
+++ src/main/java/h10/Main.java
@@ -16,12 +16,17 @@ public class Main {
     public static void main(String[] args) {
         ListItem<Integer> lst1 = listProvider(new Integer[]{1,2,3,4});
         ListItem<Integer> lst2 = listProvider(new Integer[]{3,4,5,6});
+        ListItem<Integer> lst3 = listProvider(new Integer[]{3,4,5,7});
         MySet<Integer> set1 = new MySet<>(lst1, Comparator.naturalOrder());
         MySet<Integer> set2 = new MySet<>(lst2);
-        MySet<Integer> set3 = set1.unionInPlace(set2);
+        MySet<Integer> set3 = new MySet<>(lst3);
+        MyLinkedList<MySet<Integer>> sets = new MyLinkedList<>(new ListItem<>(set2));
+        sets.add(set3);
+        MySet<Integer> set4 = set1.intersectionAsCopy(sets);
+        System.out.println(set4.toString());
         System.out.println(set3.toString());
         System.out.println(set2.toString());
-
+        System.out.println(set1.toString());
 
     }
 
