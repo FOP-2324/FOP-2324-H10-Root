18c7653b36f8acea889d08998805f003ae02f44a
LWcoder02 <larsw02@gmail.com>
2023-09-29 09:19:59 +0200

    Added Tuple class

---
+++ src/main/java/h10/Tuple.java
@@ -0,0 +1,25 @@
+package h10;
+
+public class Tuple<T1, T2> {
+
+    private final T1 FIRST_ITEM;
+    private final T2 SECOND_ITEM;
+
+    public Tuple(T1 t1, T2 t2) {
+        this.FIRST_ITEM = t1;
+        this.SECOND_ITEM = t2;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.FIRST_ITEM + ", " + this.SECOND_ITEM + ")";
+    }
+
+    public T1 getFirstItem() {
+        return this.FIRST_ITEM;
+    }
+
+    public T2 getSecondItem() {
+        return this.SECOND_ITEM;
+    }
+}
