4837e08c2b695d48891415094c3ec9cc47b9d4da
zentox <zentox@users.noreply.github.com>
2023-10-28 14:53:12 +0200

    ToString, equals

--- src/main/java/h10/AbstractMySet.java
+++ src/main/java/h10/AbstractMySet.java
@@ -1,6 +1,7 @@
 package h10;
 
 import java.util.Comparator;
+import java.util.Objects;
 
 public abstract class AbstractMySet<T> implements MySet<T> {
 
@@ -23,5 +24,30 @@ public abstract class AbstractMySet<T> implements MySet<T> {
             }
             current.next = new ListItem<>(e);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractMySet<?> that = (AbstractMySet<?>) o;
+        return Objects.equals(head, that.head) && Objects.equals(cmp, that.cmp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(head, cmp);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        ListItem<T> current = head;
+        while (current != null) {
+            sb.append(current.key).append(" -> ");
+            current = current.next;
+        }
+        sb.append("null");
+        return sb.toString();
     }
 }
