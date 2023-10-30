9d9efb43efaefb320cc47c525ad8783abcc568b2
LWcoder02 <larsw02@gmail.com>
2023-10-30 09:50:36 +0100

    Made class abstract and added helper methods

--- src/main/java/h10/MySet.java
+++ src/main/java/h10/MySet.java
@@ -1,19 +1,88 @@
 package h10;
 
+import java.util.Objects;
 import java.util.function.Predicate;
 
-public interface MySet<T> {
+public abstract class MySet<T> {
 
-    void add(T e);
-    
-    MySet<T> makeSubset(Predicate<? super T> pred);
+    protected ListItem<T> head;
 
-    MySet<T> difference(MySet<T> other);
+    protected MySet(ListItem<T> head) {
+        this.head = head;
+    }
 
-    MySet<T> intersection(ListItem<MySet<T>> others);
 
-    MySet<T> union(ListItem<MySet<T>> others);
+    protected boolean contains(ListItem<T> set, T key) {
+        for(ListItem<T> p = set; p != null; p = p.next) {
+            if(p.key == key) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    MySet<Tuple<T, ListItem<T>>> disjointUnion(ListItem<MySet<T>> others, MySet<T> indexes);
+
+    protected ListItem<T> getListOfItemsInSet(ListItem<T> lst1, ListItem<T> lst2) {
+        ListItem<T> head = new ListItem<>();
+        ListItem<T> tail = head;
+        for(ListItem<T> p = lst2; p != null; p = p.next) {
+            for(ListItem<T> curr = lst1; curr != null; curr = curr.next) {
+                if(p.equals(curr)) {
+                    tail.next = new ListItem<T>(curr.key);
+                    tail = tail.next;
+                }
+            }
+        }
+        return head.next;
+    }
+
+    void add(T e) {
+
+    }
+
+    public ListItem<T> getHead() {
+        return this.head;
+    }
+
+    public MySet<T> makeSubset(Predicate<? super T> pred) {return null;}
+
+    public MySet<T> difference(MySet<T> other) {return null;}
+
+    public MySet<T> intersection(ListItem<MySet<T>> others) {return null;}
+
+    public MySet<T> union(ListItem<MySet<T>> others) {return null;}
+
+    public MySet<Tuple<T, ListItem<T>>> disjointUnion(ListItem<MySet<T>> others, MySet<T> indexes) {return null;}
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractMySet<?> that = (AbstractMySet<?>) o;
+        return Objects.equals(head, that.head);
+    }
+
+
+
+    /*
+    @Override
+    public int hashCode() {
+        return Objects.hash(head, cmp);
+    }
+    */
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        ListItem<T> current = head;
+        while (current != null) {
+            sb.append(current.key).append(" -> ");
+            current = current.next;
+        }
+        sb.append("null");
+        return sb.toString();
+    }
 
 }
