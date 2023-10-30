cba1585467b2ffcfb9c342b885d17451bd0cf101
LWcoder02 <larsw02@gmail.com>
2023-10-30 09:51:49 +0100

    Implemented all methods

--- src/main/java/h10/MySetAsCopy.java
+++ src/main/java/h10/MySetAsCopy.java
@@ -3,10 +3,11 @@ package h10;
 import java.util.Comparator;
 import java.util.function.Predicate;
 
-public class MySetAsCopy<T> extends AbstractMySet<T> {
+public class MySetAsCopy<T> extends MySet<T> {
 
-    protected MySetAsCopy(Comparator<? super T> cmp) {
-        super(cmp);
+
+    protected MySetAsCopy(ListItem<T> head) {
+        super(head);
     }
 
     @Override
@@ -19,8 +20,7 @@ public class MySetAsCopy<T> extends AbstractMySet<T> {
                 tail = tail.next;
             }
         }
-        AbstractMySet<T> subSet = new MySetAsCopy<>(this.cmp);
-        subSet.head = head.next;
+        MySet<T> subSet = new MySetAsCopy<>(head.next);
         return subSet;
     }
 
@@ -33,22 +33,118 @@ public class MySetAsCopy<T> extends AbstractMySet<T> {
             tail = tail.next = new ListItem<T>(p.key);
         }
 
+        ListItem<T> p = other.getHead();
 
-        return null;
+        while(p != null) {
+            ListItem<T> current = head.next;
+            ListItem<T> prev = head;
+
+            while(current != null && !p.equals(current)) {
+                current = current.next;
+                prev = prev.next;
+            }
+
+            if(current != null) {
+                prev.next = current.next;
+                current.next = null;
+            }
+            p = p.next;
+        }
+
+
+        return new MySetAsCopy<>(head.next);
     }
 
     @Override
     public MySet<T> intersection(ListItem<MySet<T>> others) {
-        return null;
+        ListItem<T> head = new ListItem<T>();
+        ListItem<T> tail = head;
+
+        if(this.head == null) {
+            return new MySetAsCopy<T>(null);
+        }
+
+        for(ListItem<T> p = this.head; p != null; p = p.next) {
+            tail = tail.next = new ListItem<T>(p.key);
+        }
+
+        for(ListItem<MySet<T>> set = others; set != null; set = set.next) {
+            if(set.key.getHead() == null) {
+                return new MySetAsCopy<>(null);
+            }
+
+            ListItem<T> intersection = getListOfItemsInSet(head.next, set.key.getHead());
+
+            ListItem<T> current = head.next;
+            ListItem<T> prev = head;
+
+            while(current != null) {
+                if(!contains(intersection, current.key)) {
+                    prev.next = current.next;
+                    current.next = null;
+                    current = prev.next;
+                }
+                else {
+                    current = current.next;
+                    prev = prev.next;
+                }
+            }
+
+        }
+
+        return new MySetAsCopy<>(head.next);
     }
 
     @Override
     public MySet<T> union(ListItem<MySet<T>> others) {
-        return null;
+        ListItem<T> head = new ListItem<T>();
+        ListItem<T> tail = head;
+
+        for(ListItem<T> p = this.head; p != null; p = p.next) {
+            tail = tail.next = new ListItem<T>(p.key);
+        }
+
+        for(ListItem<MySet<T>> set = others; set != null; set = set.next) {
+            ListItem<T> p = set.key.getHead();
+
+            while(p != null) {
+                ListItem<T> next = p.next;
+
+                if(!contains(head.next, p.key)) {
+                    tail = tail.next = new ListItem<>(p.key);
+                }
+            }
+        }
+
+        return new MySetAsCopy<>(head.next);
     }
 
     @Override
     public MySet<Tuple<T, ListItem<T>>> disjointUnion(ListItem<MySet<T>> others, MySet<T> indexes) {
-        return null;
+        ListItem<Tuple<T, ListItem<T>>> head = new ListItem<>();
+        ListItem<Tuple<T, ListItem<T>>> tail = head;
+
+        ListItem<T> index = indexes.getHead();
+        ListItem<T> current = this.head;
+
+        while(current != null) {
+            tail = tail.next = new ListItem<>(new Tuple<>(index.key, new ListItem<>(current.key)));
+            current = current.next;
+        }
+
+        index = index.next;
+        ListItem<MySet<T>> set = others;
+
+        while(index != null) {
+            current = set.key.getHead();
+            while(current != null) {
+                tail = tail.next = new ListItem<>(new Tuple<>(index.key, new ListItem<>(current.key)));
+                current = current.next;
+            }
+            set = set.next;
+            index = index.next;
+        }
+
+        return new MySetAsCopy<>(head.next);
     }
 }
