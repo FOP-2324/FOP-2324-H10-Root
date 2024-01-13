6185ca00fbdefacf14fd440e7cf31358556f4d04
zentox <zentox@users.noreply.github.com>
2024-01-13 21:42:51 +0100

    Private test base rubric

---
+++ src/graderPrivate/java/h10/H10_RubricProviderPrivate.java
@@ -0,0 +1,18 @@
+package h10;
+
+import h10.rubric.H10_RubricProvider;
+
+/**
+ * The rubric provider for the private tests.
+ *
+ * @author Nhan Huynh
+ */
+public class H10_RubricProviderPrivate extends H10_RubricProvider {
+
+    /**
+     * Creates a new {@link H10_RubricProviderPrivate}.
+     */
+    public H10_RubricProviderPrivate() {
+        super("Private Tests");
+    }
+}
